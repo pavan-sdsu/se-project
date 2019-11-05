b452d74a4c7470c8ea70780bfc056567f6d8b234
pavan <pavan1645@gmail.com>
2019-11-04 22:45:16 -0800

    Added reservationservice

--- src/main/java/com/se/project/AppConfig.java
+++ src/main/java/com/se/project/AppConfig.java
@@ -1,8 +1,11 @@
 package com.se.project;
 
 import com.se.project.interceptors.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -10,5 +13,16 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/");
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
+			}
+		};
 	}
 }
